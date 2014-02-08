c6da6e3873c86a9a3355754908cc15704e634f82
Sonal Raj <sonalraj1990@gmail.com>
2014-02-08 13:17:02 +0530

    Initiated source

---
+++ src/strand.java
@@ -0,0 +1,9 @@
+/* Strand.java
+ * -------------
+ * A stand-alone library to perform Random String operations. The format for usage 
+ * and detailed guidelines are mentioned in the manual.txt file.
+ *
+ * Author: Sonal Raj
+ * 
+ */
+
