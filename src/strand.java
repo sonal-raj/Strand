e342ab3dbe07060e8e5a9f1da1e3f9edd41c3a40
Sonal Raj <sonalraj1990@gmail.com>
2014-03-17 03:25:46 +0530

    added ASCII support

--- src/strand.java
+++ src/strand.java
@@ -6,8 +6,6 @@
  * Author: Sonal Raj
  * Use: "import strand"
  */
-
-
 /*#########################################################################*/
 /*#########################################################################*/
 			//CODE IS UNDER GUIDE REVIEW. WILL BE UPDATED SOON.
@@ -53,8 +51,36 @@ public class strand
 	}
 
 	//generate random string of any length, all ASCII
+	public static void rand(boolean ASCII)
+	{
+		Random random = new Random();
+		int len  = random.nextInt(1000);
+		StringBuilder s = new StringBuilder();
+		for(int i = 32;i<=126;i++)
+			s.append((char)i);
+		char[] alpha = s.toCharArray();
+
+		StringBuilder str = new StringBuilder();
+		for(int i = 0; i<len;i++)
+			str.append(alpha[random.nextInt(alpha.length)]);
+		return str.toString();
+	}
 
 	//generate random string of specifoed length, all ASCII
+	public static void rand(int len, boolean ASCII)
+	{
+		Random random = new Random();
+
+		StringBuilder s = new StringBuilder();
+		for(int i = 32;i<=126;i++)
+			s.append((char)i);
+		char[] alpha = s.toCharArray();
+
+		StringBuilder str = new StringBuilder();
+		for(int i = 0; i<len;i++)
+			str.append(alpha[random.nextInt(alpha.length)]);
+		return str.toString();
+	}
 
 	//generate random string of any length, all Unicode
 
@@ -64,8 +90,8 @@ public class strand
 	public static void main(String[] args) {
 		System.out.println(rand(5));
 		System.out.println(rand(10));
-		System.out.println(rand(20));
-		System.out.println(rand(40));
-		System.out.println(rand(100));
+		System.out.println(rand(true));
+		System.out.println(rand(40,true));
+		System.out.println(rand(100, true));
 	}
 }
